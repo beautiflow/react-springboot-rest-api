a706859b064db562803da3eeab4c03ccf896bb5b
beautiflow <1114ny@naver.com>
2023-04-20 10:26:40 +0900

    feat: 주문 아이템 엔티티 수정

--- src/main/java/com/example/gccoffee2/model/OrderItem.java
+++ src/main/java/com/example/gccoffee2/model/OrderItem.java
@@ -2,5 +2,5 @@ package com.example.gccoffee2.model;
 
 import java.util.Objects;
 import java.util.UUID;
-public record OrderItem(UUID productId, Category category, long price, int quantity) {
+public class OrderItem {
         }
