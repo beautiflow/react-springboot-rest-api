521964d374dde492f4068e1de2edecbd12f04358
beautiflow <1114ny@naver.com>
2023-04-27 08:32:17 +0900

    feat: front gc coffee 및 에러 수정

---
+++ src/main/java/com/example/gccoffee2/GcCoffee2Application.java
@@ -0,0 +1,13 @@
+package com.example.gccoffee2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcCoffee2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GcCoffee2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gccoffee2/GcCoffee2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gccoffee2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GcCoffee2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
